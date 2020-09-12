cb4f29a671ff477d581a944a612c6db0f485f952
Jeri Ramadhan <jeriramadhan25@gmail.com>
2020-09-12 09:16:50 +0700

    tambah koneksi

--- koneksi.php
+++ koneksi.php
@@ -2,9 +2,9 @@
 	function koneksidb()
 	{ 
 		$host = 'localhost';
-		$database = 'pm';
-		$user = 'root';
-		$password = '';
+		$database = 'u8961758_pm';
+		$user = 'u8961758_jeri';
+		$password = 'jerijeri';
 		error_reporting(E_ALL ^ E_DEPRECATED);
 		$link = mysql_connect($host,$user,$password);
 		mysql_select_db($database,$link);
